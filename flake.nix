6fba7fb93f3c0b012ea23a6e42a44343688d95ed
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-11-25 17:15:26 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "chir.rs website";
+
+  inputs = {
+    cargo2nix = {
+      url = "github:cargo2nix/cargo2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.rust-overlay.follows = "rust-overlay";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = ["x86_64-linux" "aarch64-linux"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      } @ args: let
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          packageFun = import ./Cargo.nix;
+          rustChannel = "1.74.0";
+          packageOverrides = pkgs: pkgs.rustBuilder.overrides.all;
+        };
+      in {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [
+            inputs.rust-overlay.overlays.default
+            inputs.cargo2nix.overlays.default
+          ];
+        };
+        devshells.default.devshell.packages = with pkgs; [
+          (rust-bin.nightly.latest.default.override {
+            extensions = ["rust-src"];
+          })
+          inputs.cargo2nix.packages.${system}.cargo2nix
+          clang
+          cargo-deny
+          cargo-crev
+        ];
+        formatter = pkgs.alejandra;
+        packages.chir-rs = rustPkgs.workspace.chir-rs {};
+      };
+      flake = {
+        hydraJobs = {
+          inherit (inputs.self) devShells packages formatter;
+        };
+      };
+    };
+}
