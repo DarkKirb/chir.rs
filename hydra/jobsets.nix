3a7b2544437fa131d282b6da294a00400247e84b
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-02-25 08:42:26 +0100

    switch to github

--- hydra/jobsets.nix
+++ hydra/jobsets.nix
@@ -5,51 +5,16 @@
 let
   pkgs = import nixpkgs { };
   prs = builtins.fromJSON (builtins.readFile prsJSON);
-  jobsets =
-    (builtins.listToAttrs (
-      pkgs.lib.attrsets.mapAttrsToList (_: info: {
-        name = "pr${toString info.number}";
-        value = {
-          enabled = info.state == "open";
-          hidden = info.state != "open";
-          description = "PR ${toString info.number}: ${info.title}";
-          nixexprinput = "chir-rs";
-          nixexprpath = "hydra/default.nix";
-          checkinterval = 3600;
-          schedulingshares = 100;
-          enableemail = false;
-          emailoverride = "";
-          keepnr = 1;
-          inputs = {
-            chir-rs = {
-              type = "git";
-              value = "${info.head.repo.clone_url} ${info.head.ref}";
-              emailresponsible = false;
-            };
-            gitea_status_repo = {
-              type = "string";
-              value = "chir-rs";
-            };
-            gitea_repo_owner = {
-              type = "string";
-              value = info.head.repo.owner.login;
-            };
-            gitea_repo_name = {
-              type = "string";
-              value = info.head.repo.name;
-            };
-          };
-        };
-      }) prs
-    ))
-    // {
-      chir-rs = {
-        enabled = 1;
-        hidden = false;
-        description = "Current nixos config";
+  jobsets = builtins.listToAttrs (
+    pkgs.lib.attrsets.mapAttrsToList (_: info: {
+      name = "pr${toString info.number}";
+      value = {
+        enabled = info.state == "open";
+        hidden = info.state != "open";
+        description = "PR ${toString info.number}: ${info.title}";
         nixexprinput = "chir-rs";
         nixexprpath = "hydra/default.nix";
-        checkinterval = 0;
+        checkinterval = 3600;
         schedulingshares = 100;
         enableemail = false;
         emailoverride = "";
@@ -57,25 +22,27 @@ let
         inputs = {
           chir-rs = {
             type = "git";
-            value = "https://git.chir.rs/darkkirb/chir-rs main";
+            value = "${info.head.repo.clone_url} ${info.head.ref}";
             emailresponsible = false;
           };
-          gitea_status_repo = {
+          github_input = {
             type = "string";
-            value = "chir-rs";
+            value = "chir.rs";
           };
-          gitea_repo_owner = {
+          github_repo_owner = {
             type = "string";
-            value = "Darkkirb";
+            value = info.head.repo.owner.login;
           };
-          gitea_repo_name = {
+          github_repo_name = {
             type = "string";
-            value = "chir-rs";
+            value = info.head.repo.name;
           };
         };
       };
-    };
+    }) prs
+  );
 in
 {
   jobsets = pkgs.writeText "jobsets.json" (builtins.toJSON jobsets);
 }
+
